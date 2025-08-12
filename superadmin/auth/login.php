ed500bf59a6dcbbcdb5497e400776a23a4aaa11e
Prince8311 <sourishmondal.vizac@gmail.com>
2025-08-12 22:46:31 +0530

    feat: authentication

--- superadmin/auth/login.php
+++ superadmin/auth/login.php
@@ -94,7 +94,7 @@ if ($requestMethod == 'POST') {
                                             </body>
                                         </html>';
                             $mail->send();
-                            $updateSql = "UPDATE `users` SET `mail_otp`='$otp' WHERE `id` = '$userId'";
+                            $updateSql = "UPDATE `admin_users` SET `mail_otp`='$otp' WHERE `id` = '$userId'";
                             $updateResult = mysqli_query($conn, $updateSql);
 
                             if ($result) {
