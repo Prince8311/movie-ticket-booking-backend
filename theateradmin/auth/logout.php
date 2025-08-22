e45908d120ae7d340c3c916f44844c80f203a4b2
Prince8311 <sourishmondal.vizac@gmail.com>
2025-08-23 03:27:43 +0530

    feat: authentication

---
+++ theateradmin/auth/logout.php
@@ -0,0 +1,55 @@
+<?php 
+
+session_start();
+header('Access-Control-Allow-Origin: http://localhost:3000');
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
+
+$requestMethod = $_SERVER["REQUEST_METHOD"];
+
+if ($requestMethod == 'OPTIONS') {
+    header('HTTP/1.1 200 OK');
+    exit();
+}
+
+require "../../utils/middleware.php";
+
+$authResult = authenticateRequest();
+
+if (!$authResult['authenticated']) {
+    $data = [
+        'status' => $authResult['status'],
+        'message' => $authResult['message']
+    ];
+    header("HTTP/1.0 " . $authResult['status']);
+    echo json_encode($data);
+    exit;
+}
+
+if ($requestMethod == 'POST') {
+    require "../../_db-connect.php";
+    global $conn;
+
+    $token = $authResult['token'];
+    $sql = "UPDATE `theater_users` SET `token`= NULL WHERE `token`='$token'";
+    $result = mysqli_query($conn, $sql);
+    session_destroy();
+    setcookie("authToken", "", time() - 3600, "/", ".ticketbay.in", true, true);
+    $data = [
+        'status' => 200,
+        'message' => 'Logged out successfylly.',
+    ];
+    header("HTTP/1.0 200 Logged Out");
+    echo json_encode($data);
+} else {
+    $data = [
+        'status' => 405,
+        'message' => $requestMethod . ' Method Not Allowed',
+    ];
+    header("HTTP/1.0 405 Method Not Allowed");
+    echo json_encode($data);
+}
+
+?>
