3f760d087fb6eb5ed39933996a7a06af10157a32
Prince8311 <sourishmondal.vizac@gmail.com>
2025-08-30 01:18:33 +0530

    feat: section row setting

---
+++ superadmin/api/screens/settings-forms/step-1/fetch-sections.php
@@ -0,0 +1,51 @@
+<?php 
+
+session_start();
+
+header('Access-Control-Allow-Origin: http://localhost:3000');
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
+
+$requestMethod = $_SERVER["REQUEST_METHOD"];
+
+if ($requestMethod == 'OPTIONS') {
+    header('HTTP/1.1 200 OK');
+    exit();
+}
+
+require "../../../../../utils/middleware.php";
+
+$authResult = authenticateRequest();
+
+if (!$authResult['authenticated']) {
+    $data = [
+        'status' => $authResult['status'],
+        'message' => $authResult['message']
+    ];
+    header("HTTP/1.0 " . $authResult['status']);
+    echo json_encode($data);
+    exit;
+}
+
+if ($requestMethod == 'GET') {
+
+    $data = [
+        'status' => 200,
+        'message' => 'Screen sections fetched successfully.',
+        'theaterName' => 'dvdsvsvb'
+    ];
+    header("HTTP/1.0 200 OK");
+    echo json_encode($data);
+
+} else {
+    $data = [
+        'status' => 405,
+        'message' => $requestMethod . ' Method Not Allowed',
+    ];
+    header("HTTP/1.0 405 Method Not Allowed");
+    echo json_encode($data);
+}
+
+?>
