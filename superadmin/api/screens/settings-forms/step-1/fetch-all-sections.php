ba60e4a7c7526995dd63765c563757e143a0932b
Prince8311 <sourishmondal.vizac@gmail.com>
2025-08-30 01:14:20 +0530

    feat: section row setting

--- superadmin/api/screens/settings-forms/step-1/fetch-all-sections.php
+++ superadmin/api/screens/settings-forms/step-1/fetch-all-sections.php
@@ -2,7 +2,7 @@
 
 session_start();
 
-header('Access-Control-Allow-Origin: http://localhost:3000');
+header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Credentials: true');
 header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
 header('Content-Type: application/json');
