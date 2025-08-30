88006a72bc238b4bd892b0daaa354f7184d853a9
Prince8311 <sourishmondal.vizac@gmail.com>
2025-08-31 02:36:41 +0530

    feat: section row setting

---
+++ superadmin/api/screens/settings-forms/step-2/fetch-rowno-sectionwise.php
@@ -0,0 +1,80 @@
+<?php 
+
+session_start();
+
+header('Access-Control-Allow-Origin: http://localhost:3000');
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
+
+$requestMethod = $_SERVER["REQUEST_METHOD"];
+
+if ($requestMethod == 'OPTIONS') {
+    header('HTTP/1.1 200 OK');
+    exit();
+}
+
+require "../../../../../utils/middleware.php";
+
+$authResult = authenticateRequest();
+
+if (!$authResult['authenticated']) {
+    $data = [
+        'status' => $authResult['status'],
+        'message' => $authResult['message']
+    ];
+    header("HTTP/1.0 " . $authResult['status']);
+    echo json_encode($data);
+    exit;
+}
+
+if ($requestMethod == 'GET') {
+    require "../../../../../_db-connect.php";
+    global $conn;
+
+    if (isset($_GET['theaterName']) && isset($_GET['screen']) && isset($_GET['screenId']) && isset($_GET['section'])) {
+        $theaterName = mysqli_real_escape_string($conn, $_GET['theaterName']);
+        $screen = mysqli_real_escape_string($conn, $_GET['screen']);
+        $screenId = mysqli_real_escape_string($conn, $_GET['screenId']);
+        $section = mysqli_real_escape_string($conn, $_GET['section']);
+        $sql = "SELECT `id`, `section`, `row` FROM `screen_sections` WHERE `theater_name`='$theaterName' AND `screen`='$screen' AND `screen_id`='$screenId' AND `section`='$section'";
+        $result = mysqli_query($conn, $sql);
+
+        if ($result) {
+            $rowData = mysqli_fetch_assoc($result);
+            $noOfRows = $rowData['row'];
+
+            $data = [
+                'status' => 200,
+                'message' => 'No of rows fetched.',
+                'noOfRows' => $noOfRows
+            ];
+            header("HTTP/1.0 200 OK");
+            echo json_encode($data);
+        } else {
+            $data = [
+                'status' => 500,
+                'message' => 'Database error: ' . $error
+            ];
+            header("HTTP/1.0 500 Internal Server Error");
+            echo json_encode($data);
+        }
+    }  else {
+        $data = [
+            'status' => 400,
+            'message' => 'Parameters is missing.'
+        ];
+        header("HTTP/1.0 400 Bad Request");
+        echo json_encode($data);
+    }
+} else {
+    $data = [
+        'status' => 405,
+        'message' => $requestMethod . ' Method Not Allowed',
+    ];
+    header("HTTP/1.0 405 Method Not Allowed");
+    echo json_encode($data);
+}
+
+?>
