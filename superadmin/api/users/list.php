140165eb72ad954c3cad6e74f377ddbd10b777db
Prince8311 <sourishmondal.vizac@gmail.com>
2025-10-07 22:13:09 +0530

    feat: fetch users

--- superadmin/api/users/list.php
+++ superadmin/api/users/list.php
@@ -33,14 +33,24 @@ if ($requestMethod == 'GET') {
     require "../../../_db-connect.php";
     global $conn;
 
+    $limit = 10;
+    $page = isset($_GET['page']) && is_numeric($_GET['page']) && $_GET['page'] > 0
+        ? (int)$_GET['page']
+        : 1;
+    $offset = ($page - 1) * $limit;
     $sql = "SELECT `name`, `phone`, `email`, `city` FROM `users`";
     $result = mysqli_query($conn, $sql);
+    $totalUser = mysqli_num_rows($result);
+    $limitSql = "SELECT `name`, `phone`, `email`, `city` FROM `users` LIMIT $limit OFFSET $offset";
+    $limitResult = mysqli_query($conn, $limitSql);
 
     if($result) {
-        $users = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        $users = mysqli_fetch_all($limitResult, MYSQLI_ASSOC);
         $data = [
             'status' => 200,
             'message' => 'All users fetched successfully.',
+            'totalCount' => $totalUser,
+            'currentPage' => $page,
             'users' => $users
         ];
         header("HTTP/1.0 200 Users");
