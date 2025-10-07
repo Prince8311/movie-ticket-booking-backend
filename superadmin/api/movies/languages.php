d96999899f7234ff5dadec92668b056dbc0897d5
Prince8311 <sourishmondal.vizac@gmail.com>
2025-10-07 20:32:50 +0530

    feat: fetch languages

---
+++ superadmin/api/movies/languages.php
@@ -0,0 +1,65 @@
+<?php 
+
+session_start();
+
+header('Access-Control-Allow-Origin: http://localhost:3000');
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
+
+$requestMethod = $_SERVER["REQUEST_METHOD"];
+
+if ($requestMethod == 'OPTIONS') {
+    header('HTTP/1.1 200 OK');
+    exit();
+}
+
+require "../../../utils/middleware.php";
+
+$authResult = authenticateRequest();
+
+if (!$authResult['authenticated']) {
+    $data = [
+        'status' => $authResult['status'],
+        'message' => $authResult['message']
+    ];
+    header("HTTP/1.0 " . $authResult['status']);
+    echo json_encode($data);
+    exit;
+}
+
+if ($requestMethod == 'GET') {
+    require "../../../_db-connect.php";
+    global $conn;
+
+    $sql = "SELECT `language` FROM `movie_languages`";
+    $result = mysqli_query($conn, $sql);
+
+    if($result) {
+        $languages = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        $data = [
+            'status' => 200,
+            'message' => 'Languages fetched successfully.',
+            'languages' => $languages
+        ];
+        header("HTTP/1.0 200 Languages");
+        echo json_encode($data);
+    } else {
+        $data = [
+            'status' => 500,
+            'message' => 'Database error: ' . $error
+        ];
+        header("HTTP/1.0 500 Internal Server Error");
+        echo json_encode($data);
+    }
+} else {
+    $data = [
+        'status' => 405,
+        'message' => $requestMethod . ' Method Not Allowed',
+    ];
+    header("HTTP/1.0 405 Method Not Allowed");
+    echo json_encode($data);
+}
+
+?>
