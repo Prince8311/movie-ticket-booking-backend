530337b7e4c7f2c6b300231c484bf27a19da59a2
Prince8311 <sourishmondal.vizac@gmail.com>
2025-10-12 00:29:34 +0530

    style: dashboard

--- superadmin/auth/check-auth.php
+++ superadmin/auth/check-auth.php
@@ -1,5 +1,15 @@
 <?php 
 
+ini_set('session.cookie_samesite', 'None');
+ini_set('session.cookie_secure', 'true'); // only if using HTTPS
+session_set_cookie_params([
+    'lifetime' => 86400,
+    'path' => '/',
+    'domain' => '.ticketbay.in',  // important for cross-subdomain (superadmin.ticketbay.in, api.ticketbay.in, etc.)
+    'secure' => true,              // must be true if SameSite=None
+    'httponly' => true,
+    'samesite' => 'None'
+]);
 session_start();
 header('Access-Control-Allow-Origin: http://localhost:3000');
 header('Access-Control-Allow-Credentials: true');
