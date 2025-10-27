0334a21cf79a045b5c925bc26bdd56c1f5e3b675
Prince8311 <sourishmondal.vizac@gmail.com>
2025-10-27 22:49:19 +0530

    feat: commission slabs

---
+++ superadmin/api/commission_slabs/list.php
@@ -0,0 +1,70 @@
+<?php
+
+require "../../../utils/headers.php";
+
+if ($requestMethod == 'GET') {
+    require "../../../_db-connect.php";
+    global $conn;
+
+    if (isset($_GET['type'])) {
+        $type = mysqli_real_escape_string($conn, $_GET['type']);
+
+        if ($type == "admin") {
+            $sql = "SELECT * FROM `admin_commission_slabs`";
+            $result = mysqli_query($conn, $sql);
+
+            if ($result) {
+                $slabs = mysqli_fetch_all($result, MYSQLI_ASSOC);
+                $data = [
+                    'status' => 200,
+                    'message' => 'Admin slabs fetched.',
+                    'slabs' => $slabs
+                ];
+                header("HTTP/1.0 200 OK");
+                echo json_encode($data);
+            } else {
+                $data = [
+                    'status' => 500,
+                    'message' => 'Database error: ' . $error
+                ];
+                header("HTTP/1.0 500 Internal Server Error");
+                echo json_encode($data);
+            }
+        } else if ($type == "theater") {
+            $sql = "SELECT * FROM `theater_commission_slabs`";
+            $result = mysqli_query($conn, $sql);
+
+            if ($result) {
+                $slabs = mysqli_fetch_all($result, MYSQLI_ASSOC);
+                $data = [
+                    'status' => 200,
+                    'message' => 'Theater slabs fetched.',
+                    'slabs' => $slabs
+                ];
+                header("HTTP/1.0 200 OK");
+                echo json_encode($data);
+            } else {
+                $data = [
+                    'status' => 500,
+                    'message' => 'Database error: ' . $error
+                ];
+                header("HTTP/1.0 500 Internal Server Error");
+                echo json_encode($data);
+            }
+        }
+    } else {
+        $data = [
+            'status' => 400,
+            'message' => 'No parameter found',
+        ];
+        header("HTTP/1.0 400 No parameter");
+        echo json_encode($data);
+    }
+} else {
+    $data = [
+        'status' => 405,
+        'message' => $requestMethod . ' Method Not Allowed',
+    ];
+    header("HTTP/1.0 405 Method Not Allowed");
+    echo json_encode($data);
+}
---
+++ superadmin/api/commission_slabs/update.php
@@ -0,0 +1,18 @@
+<?php 
+
+require "../../../utils/headers.php";
+
+if ($requestMethod == 'POST') {
+    require "../../../_db-connect.php";
+    global $conn;
+} else {
+    $data = [
+        'status' => 405,
+        'message' => $requestMethod . ' Method Not Allowed',
+    ];
+    header("HTTP/1.0 405 Method Not Allowed");
+    echo json_encode($data);
+}
+
+
+?>
