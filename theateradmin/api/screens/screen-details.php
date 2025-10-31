9582f41135e454cbe1926be50ae3917198614781
Prince8311 <sourishmondal.vizac@gmail.com>
2025-11-01 00:17:54 +0530

    feat: theater screen details

---
+++ theateradmin/api/screens/screen-details.php
@@ -0,0 +1,74 @@
+<?php
+
+require "../../../utils/headers.php";
+require "../../../utils/middleware.php";
+
+$authResult = authenticateRequest();
+
+if (!$authResult['authenticated']) {
+    $data = [
+        'status' => $authResult['status'],
+        'message' => $authResult['message']
+    ];
+    header("HTTP/1.0 " . $authResult['status']);
+    echo json_encode($data);
+    exit;
+}
+
+if ($requestMethod == 'GET') {
+    require "../../../_db-connect.php";
+    global $conn;
+
+    if (isset($_GET['theaterName'])) {
+        $theaterName = mysqli_real_escape_string($conn, $_GET['theaterName'] ?? '');
+
+        $sql = "SELECT * FROM `registered_screens` WHERE `theater_name`='$theaterName'";
+        $result = mysqli_query($conn, $sql);
+
+        if ($result) {
+            $screenData = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+            foreach ($screenData as &$screen) {
+                $screenId = $screen['screen_id'];
+                $sectionQuery = "SELECT * FROM `screen_sections` WHERE `screen_id`='$screenId'";
+                $sectionResult = mysqli_query($conn, $sectionQuery);
+
+                if ($sectionResult) {
+                    $sections = mysqli_fetch_all($sectionResult, MYSQLI_ASSOC);
+                    $screen['sections'] = $sections;
+                } else {
+                    $screen['sections'] = [];
+                }
+            }
+
+            $data = [
+                'status' => 200,
+                'message' => 'Screen details fetched',
+                'screenDetails' => $screenData,
+            ];
+            header("HTTP/1.0 200 OK");
+            echo json_encode($data);
+        } else {
+            $data = [
+                'status' => 500,
+                'message' => 'Database error: ' . mysqli_error($conn)
+            ];
+            header("HTTP/1.0 500 Internal Server Error");
+            echo json_encode($data);
+        }
+    } else {
+        $data = [
+            'status' => 400,
+            'message' => 'Parameter is missing'
+        ];
+        header("HTTP/1.0 400 Bad Request");
+        echo json_encode($data);
+    }
+} else {
+    $data = [
+        'status' => 405,
+        'message' => $requestMethod . ' Method Not Allowed',
+    ];
+    header("HTTP/1.0 405 Method Not Allowed");
+    echo json_encode($data);
+}
