bd7c3f44b7a9305e3ac9a89f225959b080e36d1d
Prince8311 <sourishmondal.vizac@gmail.com>
2025-11-08 23:13:24 +0530

    feat: fetch movie formates

--- superadmin/api/movies/formates.php
+++ superadmin/api/movies/formates.php
@@ -1,6 +1,20 @@
 <?php
 
-require "../../../utils/headers.php";
+session_start();
+
+header('Access-Control-Allow-Origin: http://localhost:3000');
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
+
+$requestMethod = $_SERVER["REQUEST_METHOD"];
+
+if ($requestMethod == 'OPTIONS') {
+    header('HTTP/1.1 200 OK');
+    exit();
+}
+
 require "../../../utils/middleware.php";
 
 $authResult = authenticateRequest();
