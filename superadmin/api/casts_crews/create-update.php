604ffc31574a12849dfa1601f952e0154403ad4d
Prince8311 <sourishmondal.vizac@gmail.com>
2025-11-09 03:22:45 +0530

    feat: movie casts and crews

---
+++ superadmin/api/casts_crews/create-update.php
@@ -0,0 +1,32 @@
+<?php 
+
+require "../../../utils/headers.php";
+require "../../../utils/middleware.php";
+
+require "../../../utils/middleware.php";
+
+$authResult = authenticateRequest();
+
+if (!$authResult['authenticated']) {
+    $data = [
+        'status' => $authResult['status'],
+        'message' => $authResult['message']
+    ];
+    header("HTTP/1.0 " . $authResult['status']);
+    echo json_encode($data);
+    exit;
+}
+
+if ($requestMethod == 'POST') {
+    require "../../../_db-connect.php";
+    global $conn;
+} else {
+    $data = [
+        'status' => 405,
+        'message' => $requestMethod . ' Method Not Allowed',
+    ];
+    header("HTTP/1.0 405 Method Not Allowed");
+    echo json_encode($data);
+}
+
+?>
