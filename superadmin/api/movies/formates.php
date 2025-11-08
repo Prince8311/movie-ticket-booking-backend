32cd4960300309abca019951a39c006328cdbf94
Prince8311 <sourishmondal.vizac@gmail.com>
2025-11-08 23:05:35 +0530

    feat: fetch movie formates

---
+++ superadmin/api/movies/formates.php
@@ -0,0 +1,49 @@
+<?php
+
+require "../../../utils/headers.php";
+require "../../../utils/middleware.php";
+
+$authResult = authenticateRequest();
+
+if (!$authResult['authenticated']) {
+    $data = [
+        'status' => $authResult['status'],
+        'message' => $authResult['message']
+    ];
+    header("HTTP/1.0 " . $authResult['status']);
+    echo json_encode($data);
+    exit;
+}
+
+if ($requestMethod == 'GET') {
+    require "../../../_db-connect.php";
+    global $conn;
+
+    $sql = "SELECT `name` FROM `movie_formates`";
+    $result = mysqli_query($conn, $sql);
+
+    if($result) {
+        $formates = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        $data = [
+            'status' => 200,
+            'message' => 'Formates fetched successfully.',
+            'formates' => $formates
+        ];
+        header("HTTP/1.0 200 Formates");
+        echo json_encode($data);
+    } else {
+        $data = [
+            'status' => 500,
+            'message' => 'Database error: ' . $error
+        ];
+        header("HTTP/1.0 500 Internal Server Error");
+        echo json_encode($data);
+    }
+} else {
+    $data = [
+        'status' => 405,
+        'message' => $requestMethod . ' Method Not Allowed',
+    ];
+    header("HTTP/1.0 405 Method Not Allowed");
+    echo json_encode($data);
+}
---
+++ superadmin/api/screens/seat_block/create-update.php
@@ -0,0 +1,39 @@
+<?php
+
+require "../../../../utils/headers.php";
+require "../../../../utils/middleware.php";
+
+$authResult = authenticateRequest();
+
+if (!$authResult['authenticated']) {
+    $data = [
+        'status' => $authResult['status'],
+        'message' => $authResult['message']
+    ];
+    header("HTTP/1.0 " . $authResult['status']);
+    echo json_encode($data);
+    exit;
+}
+
+if ($requestMethod == 'POST') {
+    require "../../../../_db-connect.php";
+    global $conn;
+
+    $inputData = json_decode(file_get_contents("php://input"), true);
+    if (!empty($inputData)) {
+    } else {
+        $data = [
+            'status' => 400,
+            'message' => 'Empty request data'
+        ];
+        header("HTTP/1.0 400 Bad Request");
+        echo json_encode($data);
+    }
+} else {
+    $data = [
+        'status' => 405,
+        'message' => $requestMethod . ' Method Not Allowed',
+    ];
+    header("HTTP/1.0 405 Method Not Allowed");
+    echo json_encode($data);
+}
