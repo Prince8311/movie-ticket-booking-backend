c97c970469d49ce41a4cc3756a3661cf2be5abb6
Prince8311 <sourishmondal.vizac@gmail.com>
2025-11-25 00:57:51 +0530

    feat: movie upload

--- superadmin/api/movies/create.php
+++ superadmin/api/movies/create.php
@@ -22,9 +22,30 @@ if ($requestMethod == 'POST') {
     if (isset($_POST['inputs']) && isset($_FILES['image'])) {
         $inputData = json_decode($_POST['inputs'], true);
 
+        $name = mysqli_real_escape_string($conn, $inputData['name']);
+        $activity = mysqli_real_escape_string($conn, $inputData['activity']);
+        $formats = mysqli_real_escape_string($conn, $inputData['formats']);
+        $languages = mysqli_real_escape_string($conn, $inputData['languages']);
+        $releaseDate = mysqli_real_escape_string($conn, $inputData['releaseDate']);
+        $time = mysqli_real_escape_string($conn, $inputData['time']);
+        $ageCategory = mysqli_real_escape_string($conn, $inputData['ageCategory']);
+        $genres = mysqli_real_escape_string($conn, $inputData['genres']);
+        $trailer = mysqli_real_escape_string($conn, $inputData['trailer']);
+        $casts = mysqli_real_escape_string($conn, $inputData['casts']);
+        $crews = mysqli_real_escape_string($conn, $inputData['crews']);
+        $description = mysqli_real_escape_string($conn, $inputData['description']);
+
+        $imageData = $_FILES['image'];
+        $folder = "../../../posters/";
+        $timestamp = date('YmdHis');
+        $imageName = $name . $timestamp . '.png';
+        $imageDirectory = $folder . $imageName;
+        $image = getimagesize($imageData['tmp_name']);
+
         $data = [
             'status' => 200,
-            'message' => 'Movie uploaded successfully.'
+            'message' => 'Movie uploaded successfully.',
+            'imageName' => $imageName
         ];
         header("HTTP/1.0 200 Uploaded");
         echo json_encode($data);
