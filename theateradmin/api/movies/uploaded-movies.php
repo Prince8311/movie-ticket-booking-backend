ccd79880832e5eb07ffaadb14c7095851a5be188
Prince8311 <sourishmondal.vizac@gmail.com>
2025-11-29 20:39:42 +0530

    style: movie add

---
+++ theateradmin/api/movies/uploaded-movies.php
@@ -0,0 +1,61 @@
+<?php
+
+require "../../../utils/headers.php";
+require "../../../utils/middleware.php";
+
+$authResult = authenticateRequest();
+
+$authResult = authenticateRequest();
+
+if (!$authResult['authenticated']) {
+    $data = [
+        'status' => $authResult['status'],
+        'message' => $authResult['message']
+    ];
+    header("HTTP/1.0 " . $authResult['status']);
+    echo json_encode($data);
+    exit;
+}
+
+if ($requestMethod == 'GET') {
+    require "../../../_db-connect.php";
+    global $conn;
+
+    $limit = 10;
+    $page = isset($_GET['page']) && is_numeric($_GET['page']) && $_GET['page'] > 0
+        ? (int)$_GET['page']
+        : 1;
+    $offset = ($page - 1) * $limit;
+    $sql = "SELECT * FROM `movies` ORDER BY `id` DESC";
+    $result = mysqli_query($conn, $sql);
+    $totalMovies = mysqli_num_rows($result);
+    $limitSql = "SELECT * FROM `movies` ORDER BY `id` DESC LIMIT $limit OFFSET $offset";
+    $limitResult = mysqli_query($conn, $limitSql);
+
+    if($result) {
+        $movies = mysqli_fetch_all($limitResult, MYSQLI_ASSOC);
+        $data = [
+            'status' => 200,
+            'message' => 'Movies fetched.',
+            'totalCount' => $totalMovies,
+            'currentPage' => $page,
+            'movies' => $movies
+        ];
+        header("HTTP/1.0 200 Movie list");
+        echo json_encode($data);
+    } else {
+        $data = [
+            'status' => 500,
+            'message' => 'Database error: ' . $error
+        ];
+        header("HTTP/1.0 500 Internal Server Error");
+        echo json_encode($data);
+    }
+} else {
+    $data = [
+        'status' => 405,
+        'message' => $requestMethod . ' Method Not Allowed',
+    ];
+    header("HTTP/1.0 405 Method Not Allowed");
+    echo json_encode($data);
+}
