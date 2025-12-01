f1eddd4b93aaadc672d85528fbb7e2d985b79c31
Prince8311 <sourishmondal.vizac@gmail.com>
2025-12-02 03:05:45 +0530

    feat: user authentication

--- user/auth/login.php
+++ user/auth/login.php
@@ -35,12 +35,12 @@ if ($requestMethod == 'POST') {
                         ];
 
                         $jsonPayload = json_encode($payload);
-                        $randomBytes = random_bytes(64);
+                        $randomBytes = random_bytes(128);
                         $tokenData = $jsonPayload . '|' . bin2hex($randomBytes);
                         $authToken = base64_encode($tokenData);
                         $expiresAt = date("Y-m-d H:i:s", time() + 86400);
 
-                        $updateSql = "UPDATE `users` SET `expires_at`='$expiresAt' WHERE `id`='$userId'";
+                        $updateSql = "UPDATE `users` SET `auth_token`='$authToken',`expires_at`='$expiresAt' WHERE `id`='$userId'";
                         $updateResult = mysqli_query($conn, $updateSql);
 
                         if ($updateResult) {
