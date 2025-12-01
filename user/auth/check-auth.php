060480cf2ea78cb91f4ded5a7a0fb2ee3d38b3b7
Prince8311 <sourishmondal.vizac@gmail.com>
2025-12-02 04:06:06 +0530

    feat: user authentication

--- user/auth/check-auth.php
+++ user/auth/check-auth.php
@@ -21,35 +21,13 @@ if ($requestMethod == 'GET') {
     require "../../_db-connect.php";
     global $conn;
 
-    $sql = "SELECT `id`, `name`, `image`, `phone`, `email`, `status`, `ticket_booked` FROM `users` WHERE `id`='$userID'";
-    $result = mysqli_query($conn, $sql);
-    if ($result) {
-        if (mysqli_num_rows($result) > 0) {
-            $user = mysqli_fetch_assoc($result);
-            $data = [
-                'status' => 200,
-                'message' => 'Authenticated',
-                'user' => $user,
-                'tokenRefreshed' => $refreshed
-            ];
-            header("HTTP/1.0 200 Authenticated");
-            echo json_encode($data);
-        } else {
-            $data = [
-                'status' => 400,
-                'message' => 'No user found'
-            ];
-            header("HTTP/1.0 400 Not found");
-            echo json_encode($data);
-        }
-    } else {
-        $data = [
-            'status' => 500,
-            'message' => 'Database error: ' . mysqli_error($conn)
-        ];
-        header("HTTP/1.0 500 Internal Server Error");
-        echo json_encode($data);
-    }
+    $data = [
+        'status' => 200,
+        'message' => 'Authenticated',
+        'tokenRefreshed' => $refreshed
+    ];
+    header("HTTP/1.0 200 Authenticated");
+    echo json_encode($data);
 } else {
     $data = [
         'status' => 405,
