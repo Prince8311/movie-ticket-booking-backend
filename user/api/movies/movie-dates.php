f501fad6accec4c94798d31fe26c55dba1d9a303
Prince8311 <sourishmondal.vizac@gmail.com>
2025-12-31 02:10:11 +0530

    feat: user public api

--- user/api/movies/movie-dates.php
+++ user/api/movies/movie-dates.php
@@ -1,18 +1,6 @@
 <?php
 
 require "../../../utils/headers.php";
-require "../../../utils/middleware.php";
-
-$authResult = authenticateRequest();
-if (!$authResult['authenticated']) {
-    $data = [
-        'status' => $authResult['status'],
-        'message' => $authResult['message']
-    ];
-    header("HTTP/1.0 " . $authResult['status']);
-    echo json_encode($data);
-    exit;
-}
 
 if ($requestMethod == 'GET') {
     require "../../../_db-connect.php";
