fd51f7147c975b884401a66b872b3c6d96170474
Prince8311 <sourishmondal.vizac@gmail.com>
2026-01-13 03:31:15 +0530

    feat: payment

---
+++ user/api/booking/payment/payment-process.php
@@ -0,0 +1,41 @@
+<?php
+
+date_default_timezone_set('Asia/Kolkata');
+require "../../../../utils/headers.php";
+require "../../../../utils/middleware.php";
+
+$authResult = authenticateRequest();
+if (!$authResult['authenticated']) {
+    $data = [
+        'status' => $authResult['status'],
+        'message' => $authResult['message']
+    ];
+    header("HTTP/1.0 " . $authResult['status']);
+    echo json_encode($data);
+    exit;
+}
+
+if ($requestMethod == 'POST') {
+    require "../../../../_db-connect.php";
+    global $conn;
+
+    $inputData = json_decode(file_get_contents("php://input"), true);
+
+    if (!empty($inputData)) {
+        $userName = mysqli_real_escape_string($conn, $inputData['userName']);
+    } else {
+        $data = [
+            'status' => 400,
+            'message' => 'Empty request data'
+        ];
+        header("HTTP/1.0 400 Bad Request");
+        echo json_encode($data);
+    }
+} else {
+    $data = [
+        'status' => 405,
+        'message' => $requestMethod . ' Method Not Allowed',
+    ];
+    header("HTTP/1.0 405 Method Not Allowed");
+    echo json_encode($data);
+}
