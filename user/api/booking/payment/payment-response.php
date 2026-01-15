44b65eff135fc1075541565606eb14ba635f78d0
Prince8311 <sourishmondal.vizac@gmail.com>
2026-01-15 23:56:22 +0530

    feat: payment

--- user/api/booking/payment/payment-response.php
+++ user/api/booking/payment/payment-response.php
@@ -2,18 +2,6 @@
 
 date_default_timezone_set('Asia/Kolkata');
 require "../../../../utils/headers.php";
-require "../../../../utils/middleware.php";
-
-$authResult = authenticateRequest();
-if (!$authResult['authenticated']) {
-    $data = [
-        'status' => $authResult['status'],
-        'message' => $authResult['message']
-    ];
-    header("HTTP/1.0 " . $authResult['status']);
-    echo json_encode($data);
-    exit;
-}
 
 if ($requestMethod == 'POST') {
     require "../../../../_db-connect.php";
