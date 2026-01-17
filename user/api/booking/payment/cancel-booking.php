b731e874dd3872cf0d45bb81602844c970bb7ca3
Prince8311 <sourishmondal.vizac@gmail.com>
2026-01-17 23:08:57 +0530

    feat: booking cancel

--- user/api/booking/payment/cancel-booking.php
+++ user/api/booking/payment/cancel-booking.php
@@ -23,51 +23,26 @@ if ($requestMethod == 'POST') {
 
     if (!empty($inputData)) {
         $bookingId = mysqli_real_escape_string($conn, $inputData['bookingId']);
-        $userName = mysqli_real_escape_string($conn, $inputData['userName']);
 
         $bookingSql = "SELECT * FROM `online_bookings` WHERE `booking_id`='$bookingId";
         $bookingResult = mysqli_query($conn, $bookingSql);
 
-        if (!$bookingResult) {
+        if ($bookingResult) {
+            $data = [
+                'status' => 200,
+                'message' => 'Booking data',
+                'bookingId' => $bookingId,
+            ];
+            header("HTTP/1.0 200 OK");
+            echo json_encode($data);
+        } else {
             $data = [
                 'status' => 500,
                 'message' => 'Database error: ' . mysqli_error($conn)
             ];
             header("HTTP/1.0 500 Internal Server Error");
             echo json_encode($data);
-            exit;
         }
-
-        if (mysqli_num_rows($bookingResult) === 0) {
-            $data = [
-                'status' => 404,
-                'message' => 'No booking found with this id'
-            ];
-            header("HTTP/1.0 404 Not found");
-            echo json_encode($data);
-            exit;
-        }
-
-        $bookingData = mysqli_fetch_assoc($bookingResult);
-        $showTime = $bookingData['start_time'];
-        $showData = $bookingData['start_date'];
-        $amount = $bookingData['ticket_price'];
-
-        $showDateTimeStr = $showDate . ' ' . $showTime;
-        $showDateTime = DateTime::createFromFormat('d M, Y h:i A', $showDateTimeStr);
-        $currentDateTime = new DateTime('now');
-        $interval = $currentDateTime->diff($showDateTime);
-        $totalHours = ($interval->days * 24) + $interval->h + ($interval->i / 60);
-
-        $data = [
-            'status' => 200,
-            'message' => 'Booking data',
-            'bookingId' => $bookingId,
-            'amount' => $amount,
-            'timeLeft' => $totalHours,
-        ];
-        header("HTTP/1.0 200 OK");
-        echo json_encode($data);
     } else {
         $data = [
             'status' => 400,
