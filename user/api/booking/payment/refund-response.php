e6db60ba6c5f896eba4b6de0b9af34a72fbce121
Prince8311 <sourishmondal.vizac@gmail.com>
2026-01-22 02:40:37 +0530

    feat: booking cancel

--- user/api/booking/payment/refund-response.php
+++ user/api/booking/payment/refund-response.php
@@ -26,6 +26,24 @@ if ($receivedChecksum !== $calculatedChecksum) {
     exit("Invalid signature");
 }
 
+$logData = [
+    'time' => date('Y-m-d H:i:s'),
+    'ip' => $_SERVER['REMOTE_ADDR'] ?? 'unknown',
+    'headers' => [
+        'X-VERIFY' => $_SERVER['HTTP_X_VERIFY'] ?? null,
+        'X-MERCHANT-ID' => $_SERVER['HTTP_X_MERCHANT_ID'] ?? null,
+    ],
+    'raw_body' => $rawBody,
+    'payload' => $payload,
+    'code' => $code
+];
+
+file_put_contents(
+    $logFile,
+    json_encode($logData, JSON_UNESCAPED_SLASHES) . PHP_EOL,
+    FILE_APPEND | LOCK_EX
+);
+
 if (!isset($payload['response'])) {
     http_response_code(400);
     exit("Missing response");
@@ -45,24 +63,6 @@ $transactionId = $responseData['data']['transactionId'] ?? null;
 $amount = isset($responseData['data']['amount'])
     ? $responseData['data']['amount'] / 100
     : null;
-
-$logData = [
-    'time' => date('Y-m-d H:i:s'),
-    'ip' => $_SERVER['REMOTE_ADDR'] ?? 'unknown',
-    'headers' => [
-        'X-VERIFY' => $_SERVER['HTTP_X_VERIFY'] ?? null,
-        'X-MERCHANT-ID' => $_SERVER['HTTP_X_MERCHANT_ID'] ?? null,
-    ],
-    'raw_body' => $rawBody,
-    'payload' => $payload,
-    'code' => $code
-];
-
-file_put_contents(
-    $logFile,
-    json_encode($logData, JSON_UNESCAPED_SLASHES) . PHP_EOL,
-    FILE_APPEND | LOCK_EX
-);
 
 $refundSql = "SELECT * FROM `refund_history` WHERE `merchant_transaction_id`='$merchantTxnId'";
 $refundResult = mysqli_query($conn, $refundSql);
