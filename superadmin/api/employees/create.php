6cf0847c36ba666241a37327b417a3f8a814722d
Prince8311 <sourishmondal.vizac@gmail.com>
2026-01-25 17:03:51 +0530

    feat: super admin permissions

--- superadmin/api/employees/create.php
+++ superadmin/api/employees/create.php
@@ -1,24 +1,10 @@
 <?php
 
-session_start();
+require "../../../utils/headers.php";
+require "../../../utils/middleware.php";
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
-
-header('Access-Control-Allow-Origin: http://localhost:3000');
-header('Access-Control-Allow-Credentials: true');
-header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
-header('Content-Type: application/json');
-header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
-
-$requestMethod = $_SERVER["REQUEST_METHOD"];
-
-if ($requestMethod == 'OPTIONS') {
-    header('HTTP/1.1 200 OK');
-    exit();
-}
-
-require "../../../utils/middleware.php";
 
 $authResult = authenticateRequest();
 
