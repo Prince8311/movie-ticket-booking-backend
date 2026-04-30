a6a967a236190f23e4d55818902a617d93bf9c6e
Prince8311 <sourishmondal.vizac@gmail.com>
2026-04-30 23:24:01 +0530

    feat: new deployment

--- _db-connect.php
+++ _db-connect.php
@@ -6,20 +6,3 @@ $password = 'Abhay$ticketbay@2024';
 $database = "u181314319_movieTicket";
 
 $conn = mysqli_connect($server, $username, $password, $database);
-
-if (!$conn) {
-    http_response_code(500);
-    echo json_encode([
-        "status" => 500,
-        "message" => "Database connection failed",
-        "error" => mysqli_connect_error()
-    ]);
-    exit;
-}
-
-// TEMP DEBUG (remove later)
-echo json_encode([
-    "status" => 200,
-    "message" => "Database connected successfully"
-]);
-exit;
